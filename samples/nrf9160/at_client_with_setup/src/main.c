c236a058b45642595a6ffa67c8c93327449fda12
Akash Patel <akash.patel@nordicsemi.no>
2021-06-04 17:02:32 -0700

    at_client_with_setup: add setup command integration

--- samples/nrf9160/at_client_with_setup/src/main.c
+++ samples/nrf9160/at_client_with_setup/src/main.c
@@ -10,6 +10,22 @@
 #include <string.h>
 #include <drivers/clock_control.h>
 #include <drivers/clock_control/nrf_clock_control.h>
+#include <modem/at_cmd.h>
+#include <modem/at_notif.h>
+
+#define AT_XMODEM_TRACE		"AT\%XMODEMTRACE=1,2"
+#define AT_CEREG			"AT+CEREG=5"
+#define AT_CNEC				"AT+CNEC=24"
+#define AT_CSCON			"AT+CSCON=3"
+#define AT_CESQ				"AT\%CESQ=1"
+
+static const char *const at_commands[] = {
+	AT_XMODEM_TRACE,
+	AT_CEREG,
+	AT_CNEC,
+	AT_CSCON,
+	AT_CESQ
+};
 
 /**@brief Recoverable modem library error. */
 void nrf_modem_recoverable_error_handler(uint32_t err)
@@ -28,8 +44,29 @@ void enable_xtal(void)
 	(void)onoff_request(clk_mgr, &cli);
 }
 
+/* Run each of the AT Commands defined above one at a time */
+static int setup_commands(void)
+{
+	for (int i = 0; i < ARRAY_SIZE(at_commands); i++) {
+
+		if (at_cmd_write(at_commands[i], NULL, 0, NULL) != 0) {
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
 void main(void)
 {
 	enable_xtal();
-	printk("The AT host sample started\n");
+	if (setup_commands() != 0) {
+		printk("Failed to setup modem\n");
+		return -1;
+	}
+	else{
+		printk("The AT host sample started\n");
+		printk("Modem trace and notifications for CEREG, CNEC, CSCON, and CESQ are enabled\n");
+		return 0;
+	}
 }
